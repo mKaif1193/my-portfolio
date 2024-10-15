9fddfdc82203332d2630677caf94a80198f044cd
Muhammad Kaif <83498731+mKaif1193@users.noreply.github.com>
2024-10-15 12:15:13 +0500

    Final Deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,6 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-// export const metadata: Metadata = {
-//   title: "Muhammad Kaif Portfolio",
-//   description:
-//     "This site is a portfolio website showcasing some of the projects I worked on. It is a pure Next JS site which makes it very fast and responsives to all the devices, The site provides my skill set, work and what I have done in one of the easiest manner. With project descriptions and optimized for both desktop and mobile in responsive design as well. For the book is not only a declaration of personal brand, but also an entry point for potential customers and employers to educate themselves about where my work has led.",
-// };
-
 export const metadata: Metadata = {
   metadataBase: new URL(DATA.url),
   title: {
